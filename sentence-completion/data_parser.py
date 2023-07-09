002166a8df1ab9c0aeb8c4cf22dc6cb1405bf854
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-09 16:19:08 -0700

    updatd work!

--- sentence-completion/data_parser.py
+++ sentence-completion/data_parser.py
@@ -1,8 +1,11 @@
 import config
+import pickle
+import codecs
 import os
 import re
+import pandas as pd
 
-def files(directory=config.TRAINING_DATA_PATH):
+def files(directory):
     return list(os.listdir(directory))
 
 def extract_text(lines):
@@ -11,27 +14,47 @@ def extract_text(lines):
 
 def clean(text):
     words = re.findall("[a-zA-Z'-]+", text)
+    words = ["".join(word.split("'")) for word in words]
     return ' '.join(words) if words else ''
 
-def load_data(directory=config.TRAINING_DATA_PATH):
+def load_train_data(directory=config.TRAINING_DATA_PATH):
     trainingData = []
-    book = ''
-    #with open('data/sentences.txt', 'w') as output:
-    for file in files():
-        with open(f'''{directory}/{file}''', 'r') as novel:
-            text = ''
-            parseFlag = False
-            for line in novel.readlines():
-                line = line.replace("\n", " ")
-                if line and parseFlag:
-                    if line.find('----------') > -1:
-                        break
-                    text += (extract_text(line.lower()) + ' ')
-                if not parseFlag and line.find('Arthur Conan Doyle') > -1:
-                    parseFlag = True
-            book += (clean(text))
-            text = [clean(s).split(' ') for s in text.split(".")]
-            trainingData += [text]
-    print(trainingData[0])
+    with codecs.open('model/all_words.txt', "w", encoding='utf-8', errors='ignore') as out:
+        for file in files(directory):
+            with open(f'''{directory}/{file}''', 'r') as novel:
+                text = ''
+                parseFlag = False
+                for line in novel.readlines():
+                    line = line.replace("\n", " ")
+                    if line and parseFlag:
+                        if line.find('----------') > -1:
+                            break
+                        text += (extract_text(line.lower()) + ' ')
+                    if not parseFlag and line.find('Arthur Conan Doyle') > -1:
+                        parseFlag = True
+                out.write(clean(text) + ' ')
+                trainingData += [clean(s).split(' ') for s in text.split(".")]
+        pickle.dump(trainingData, open('data/trainingData.txt', "wb"), True)
+
+def load_test_data(directory=config.TEST_DATA_PATH):
+    testData = []
+    with codecs.open('model/test_words.txt', 'w', encoding='utf-8', errors='ignore') as out:
+        file = pd.read_csv(directory)
+        for i in range(len(file)):
+            row = file.iloc[i, :]
+            question = [clean(word.lower()) if word != '_____' else '_____' for word in row['question'].split(' ')]
+            testData.append({'id':row['id'],
+                             'question':question,
+                             'a':row['a)'].lower(),
+                             'b':row['b)'].lower(),
+                             'c':row['c)'].lower(),
+                             'd':row['d)'].lower(),
+                             'e':row['e)'].lower()})
+            out.write(clean(row['question'].lower()) + ' ')
+        pickle.dump(testData, open('data/testData.txt', "wb"), True)
+
+def load_data():
+    load_train_data()
+    load_test_data()
 
 load_data()
