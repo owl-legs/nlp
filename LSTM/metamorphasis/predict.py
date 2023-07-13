f44757e4ff25ebfb2f7ccb931ec65b281d1af63f
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-13 10:41:03 -0700

    model pred class updates

--- LSTM/metamorphasis/predict.py
+++ LSTM/metamorphasis/predict.py
@@ -6,14 +6,14 @@ from tensorflow.keras.models import load_model
 class TestInput():
     def __init__(self):
         self.tokenizer = pickle.load(open(config.TOKEN_DIC_OUTPUT, "rb"))
+        print(self.tokenizer)
         self.tokenKeyMap = {v:k for k,v in self.tokenizer.items()}
         self.model = load_model("metamorphasis.h5")
 
     def __predict_next_token__(self, lastToken):
         input = np.array(self.tokenizer[lastToken])
-        print(input)
-        index = self.model.predict_classes(input)
-        print(index)
+        preds = self.model.predict_classes(input)
+        index = np.argmax(preds)
         return self.tokenKeyMap[index]
     def __test__(self):
         while True:
