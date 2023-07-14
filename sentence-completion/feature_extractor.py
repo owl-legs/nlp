de3fb7bb86fef51b63299c21c51cef9bee3a1ee7
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-13 22:25:02 -0700

    directory refactor

--- sentence-completion/feature_extractor.py
+++ sentence-completion/feature_extractor.py
@@ -15,14 +15,17 @@ class FeatureExtractor:
         self.stopwords = self.__load_stop_words__()
         self.wordDic = self.__count_tokens__(open(config.CORPUS_PATH, 'r').read().split())
         self.trainingWordDic = self.__count_tokens__(pickle.load(open(config.PROCESSED_TRAIN_DATA_PATH, "rb")))
-        #self.testWordDic = self.__count_tokens__(pickle.load(open(config.PROCESSED_TEST_DATA_PATH, "rb")))
+        self.testWordDic = self.__count_tokens__(pickle.load(open(config.PROCESSED_TEST_DATA_PATH, "rb")))
 
     def extract_features(self,
-                         embedding_type):
+                         embedding_type,
+                         training=True):
         self.__test_sentence_stats__()
 
-        self.__create_embeddings__(output_path=config.EMBEDDED_TRAIN_PATH, option='word2vec')
-        #self.__create_embeddings__(output_path=config.EMBEDDED_TEST_PATH, option='word2vec', train=False)
+        if training:
+            self.__create_embeddings__(output_path=config.EMBEDDED_TRAIN_PATH, option=embedding_type, train=training)
+        else:
+            self.__create_embeddings__(output_path=config.EMBEDDED_TEST_PATH, option=embedding_type, train=training)
     def __load_stop_words__(self):
         return set(open(config.STOP_WORDS_PATH, "r").read().split(" "))
     def __count_tokens__(self, sentences):
@@ -63,10 +66,39 @@ class FeatureExtractor:
         word_vector.wv.save_word2vec_format(config.WORD2VEC_BIN_PATH, binary=True)
     def __build_one_hot_mappings__(self, dic, one_hot_file_output=config.ONE_HOT_MAP_PATH):
         oneHotMap = {}
-        wordList = dic.mostcommon()
+        wordList = dic.most_common()
         for i, word in enumerate(wordList):
             oneHotMap[word[0]] = i
+        oneHotMap['<unk>'] = len(wordList)
         pickle.dump(oneHotMap, open(one_hot_file_output, 'wb'), True)
+
+    def __embed_pre_context__(self, sentence, index, embeddingMap, option):
+        if option == 'one-hot':
+            return np.array([embeddingMap[word] if word not in self.stopwords \
+                                                   and word in embeddingMap \
+                                                   and word != "" \
+                                 else embeddingMap['<unk>'] \
+                             for word in sentence[:index]])
+        elif option == 'word2vec':
+            return np.array([embeddingMap[word] if word not in self.stopwords \
+                                            and word in embeddingMap \
+                                            and word != "" \
+                          else np.zeros(config.EMBEDDING_SIZE) \
+                      for word in sentence[:index]])
+
+    def __embed_post_context__(self, sentence, index, embeddingMap, option):
+        if option == 'one-hot':
+            return np.array([embeddingMap[word] if word not in self.stopwords \
+                                                   and word in embeddingMap \
+                                                   and word != "" \
+                                 else embeddingMap['<unk>'] \
+                             for word in sentence[:index]])
+        elif option == 'word2vec':
+            return np.array([embeddingMap[word] if word not in self.stopwords \
+                                            and word in embeddingMap \
+                                            and word != "" \
+                          else np.zeros(config.EMBEDDING_SIZE) \
+                      for word in sentence[index+1:]])
 
     def __create_embeddings__(self, output_path, option='one-hot', train=True):
 
@@ -78,7 +110,11 @@ class FeatureExtractor:
             embeddingMap = KeyedVectors.load_word2vec_format(config.WORD2VEC_BIN_PATH,
                                                              binary=True)
         else:
-            embeddingMap = pickle.load(open(filePath[option], "rb"))
+            try:
+                embeddingMap = pickle.load(open(filePath[option], "rb"))
+            except:
+                self.__build_one_hot_mappings__(dic=self.wordDic)
+                embeddingMap = pickle.load(open(filePath[option], "rb"))
 
         embeddedX, embeddedY = [], []
         counter = 0
@@ -96,18 +132,10 @@ class FeatureExtractor:
                 if validSentence:
                     midIndex = int(len(sentence) // 2)
 
-                    preContextVector = np.array([embeddingMap[word] if word not in self.stopwords \
-                                                                       and word in embeddingMap \
-                                                                       and word != "" \
-                                                     else np.zeros(config.EMBEDDING_SIZE) \
-                                                 for word in sentence[:midIndex]])
+                    preContextVector = self.__embed_pre_context__(sentence, midIndex, embeddingMap, option)
                     preContextVector = np.sum(preContextVector, axis=0)
 
-                    postContextVector = np.array([embeddingMap[word] if word not in self.stopwords \
-                                                                        and word in embeddingMap \
-                                                                        and word != "" \
-                                                      else np.zeros(config.EMBEDDING_SIZE) \
-                                                  for word in sentence[midIndex + 1:]])
+                    postContextVector = self.__embed_post_context__(sentence, midIndex, embeddingMap, option)
                     postContextVector = np.sum(postContextVector, axis=0)
 
                     if sentence[midIndex] in embeddingMap:
@@ -118,33 +146,18 @@ class FeatureExtractor:
                     embeddedX.append([preContextVector, midContextVector])
                     embeddedY.append(postContextVector)
 
-                    counter += 1
-                    if counter == 18269:
-                        postfix += 1
-                        print("Save file, postfix:", postfix)
-                        pickle.dump((np.array(embeddedX), np.array(embeddedY), len(embeddedX)), \
-                                    open('data/embedded_train_data_%d' % (postfix), 'wb'), True)
-                        embeddedX = []
-                        embeddedY = []
-                        counter = 0
+                pickle.dump((np.array(embeddedX), np.array(embeddedY), len(embeddedX)), \
+                             open('data/embedded_train_full', 'wb'), True)
 
         else:
             sentences = pickle.load(open(config.PROCESSED_TEST_DATA_PATH, "rb"))
             for testCase in sentences:
                 question = testCase['question']
                 index = question.index("_____")
-                preContextVector = np.array([embeddingMap[word] if word not in self.stopwords \
-                                                                        and word in embeddingMap \
-                                                                        and word != "" \
-                                                                 else np.zeros(config.EMBEDDING_SIZE) \
-                                                                 for word in question[:index]])
+                preContextVector = self.__embed_pre_context__(question, index, embeddingMap, option)
                 preContextVector = np.sum(preContextVector, axis=0)
 
-                postContextVector = np.array([embeddingMap[word] if word not in self.stopwords \
-                                                                         and word in embeddingMap \
-                                                                         and word != "" \
-                                                                 else np.zeros(config.EMBEDDING_SIZE) \
-                                                                 for word in question[index+1:]])
+                postContextVector = self.__embed_post_context__(question, index, embeddingMap, option)
                 postContextVector = np.sum(postContextVector, axis=0)
 
                 for option in {'a', 'b', 'c', 'd', 'e'}:
@@ -162,4 +175,4 @@ class FeatureExtractor:
 
 
 featureExtractor = FeatureExtractor()
-featureExtractor.extract_features(embedding_type='word2vec')
+featureExtractor.extract_features(embedding_type='one-hot')
--- sentence-completion/rnn.py
+++ sentence-completion/rnn.py
@@ -1,4 +1,19 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam
 
-def LSTM(x):
-    return x
+import pickle
+
+x, y, datalen = pickle.load(open('data/embedded_train_full', 'rb'))
+vocab_size = len(pickle.load(open('model/one_hot_dic.txt', 'rb')))
+
+model = Sequential()
+model.add(Embedding(vocab_size, 10, input_length=1))
+model.add(LSTM(1000, return_sequences=True))
+model.add(LSTM(1000))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(vocab_size, activation="softmax"))
+
+model.summary()
