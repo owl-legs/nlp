079b1d237a3de8fe710b06a57f10805e18f50359
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-15 14:50:45 -0700

    encoding bug fix and model update

--- sentence-completion/test.py
+++ sentence-completion/test.py
@@ -5,3 +5,6 @@ import tensorflow as tf
 
 
 model = tf.keras.models.load_model('sherlock.h5')
+embeddingMap = KeyedVectors.load_word2vec_format(config.WORD2VEC_BIN_PATH, binary=True)
+test_data = pickle.load(open(config.PROCESSED_TEST_DATA_PATH, 'rb'))
+
