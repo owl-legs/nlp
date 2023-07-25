e1b80d2ef2ab0a64428cb414db95aa3157925e7b
alexrotondo <alexandra.rotondo@ucdenver.edu>
2023-07-25 15:04:02 -0700

    interview prep

--- multiclass_confusion_matrix.py
+++ multiclass_confusion_matrix.py
@@ -3,13 +3,14 @@ def generate_predictions(n, n_classes):
     return [random.randint(1, n_classes) for _ in range(n)]
 
 model_predictions = []
-m_models = 4
+m_models = 1
 n_classes = 4
+n_samples = 10
 
 for _ in range(m_models):
-    model_predictions.append(generate_predictions(n=100, n_classes=n_classes))
+    model_predictions.append(generate_predictions(n=n_samples, n_classes=n_classes))
 
-ground_truth = generate_predictions(n=100, n_classes=n_classes)
+ground_truth = generate_predictions(n=n_samples, n_classes=n_classes)
 
 def calculate_matrix(y_pred, y_true, n_classes):
     matrix = [[0] * n_classes for _ in range(n_classes)]
@@ -22,5 +23,6 @@ def calculate_matrix(y_pred, y_true, n_classes):
 
     return matrix
 
-
+print(model_predictions[0])
+print(ground_truth)
 print(calculate_matrix(model_predictions[0], ground_truth, n_classes=n_classes))
