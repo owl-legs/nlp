650db935775f886d2c0a24cb9dbabfd2f69fb763
alexrotondo <alexandra.rotondo@ucdenver.edu>
2025-03-11 22:44:01 -0700

    work

--- embeddings/word2vec/model.py
+++ embeddings/word2vec/model.py
@@ -1,28 +1,39 @@
-from dataclasses import dataclass
 import torch.nn as nn
 
 EMBED_DIMENSION = 300
 EMBED_MAX_NORM = 1
 
-@dataclass
+
 class ContinuousBagOfWords(nn.Module):
     def __init__(self, vocab_size: int):
 
         super(ContinuousBagOfWords, self).__init__()
 
-        # embedding layer
+        """
+            embedding layer
+            -> input: BATCH_SIZE x VOCAB_SIZE x 1
+            -> output: BATCH_SIZE x EMBED_DIMMENSION
+        """
         self.embeddings = nn.Embedding(
             num_embeddings=vocab_size,
             embedding_dim=EMBED_DIMENSION,
             max_norm=EMBED_MAX_NORM,
         )
-        # linear layer
+
+        """
+            linear layer
+            -> input: BATCH_SIZE x EMBED_DIMMENSION
+            -> output: BATCH_SIZE x VOCAB_SIZE
+        """
         self.linear = nn.Linear(
             in_features=EMBED_DIMENSION,
             out_features=vocab_size,
         )
 
     def forward(self, x):
+        """
+            forward pass
+        """
         x = self.embeddings(x)
         x = x.mean(axis=1)
         x = self.linear(x)
